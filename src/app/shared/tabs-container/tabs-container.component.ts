67ebde2d203a9a6fc323a69426dbf0890e7f2205
Cristian Tudor <cristi_tcs@live.com>
2023-08-10 10:01:24 +0300

    tabs can now be set active

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -12,10 +12,22 @@ import { TabComponent } from '../tab/tab.component';
   styleUrls: ['./tabs-container.component.css'],
 })
 export class TabsContainerComponent implements AfterContentInit {
-  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent> =
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> =
     new QueryList();
 
   ngAfterContentInit() {
-    console.log(this.tabs);
+    const activeTabs = this.tabs?.filter((tab) => tab.active);
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach((tab) => {
+      tab.active = false;
+    });
+    tab.active = true;
+
+    return false;
   }
 }
