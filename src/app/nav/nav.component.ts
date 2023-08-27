ee762f855851c7fb3d21257563258a7cb87c74a1
Cristian Tudor <cristi_tcs@live.com>
2023-08-27 20:36:46 +0300

    added firebase login to logon component and firebase logout to nav component, firebase initialization starts before bootstraping the app

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -8,10 +9,19 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-  constructor(public modal: ModalService, public auth: AuthService) {}
+  constructor(
+    public modal: ModalService,
+    public auth: AuthService,
+    private afAuth: AngularFireAuth
+  ) {}
 
   openModal(event: Event) {
     event.preventDefault();
     this.modal.toggleModal('auth');
+  }
+
+  async logOut(event: Event) {
+    event.preventDefault();
+    await this.afAuth.signOut();
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, map, delay } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import {
   AngularFirestore,
@@ -14,10 +14,12 @@ import IUser from 'src/app/models/user.model';
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
   public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = db.collection<IUser>('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
   }
 
   public async createUser(userData: IUser) {
