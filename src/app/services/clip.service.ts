516259b9eba41ab0644268161c2e4bcfc148324a
Cristian Tudor <cristi_tcs@live.com>
2023-09-20 23:15:55 +0300

    created edit component for editing videos

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -3,7 +3,10 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
   DocumentReference,
+  QuerySnapshot,
 } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { switchMap, of, map } from 'rxjs';
 
 import IClip from '../models/clip.model';
 
@@ -13,11 +16,32 @@ import IClip from '../models/clip.model';
 export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>;
 
-  constructor(private db: AngularFirestore) {
+  constructor(private db: AngularFirestore, private auth: AngularFireAuth) {
     this.clipsCollection = db.collection('clips');
   }
 
   createClip(data: IClip): Promise<DocumentReference<IClip>> {
     return this.clipsCollection.add(data);
+  }
+
+  getUserClips() {
+    return this.auth.user.pipe(
+      switchMap((user) => {
+        if (!user) {
+          return of([]);
+        }
+
+        const query = this.clipsCollection.ref.where('uid', '==', user.uid);
+
+        return query.get();
+      }),
+      map((snapshot) => (snapshot as QuerySnapshot<IClip>).docs)
+    );
+  }
+
+  updateClip(id: string, title: string) {
+    return this.clipsCollection.doc(id).update({
+      title,
+    });
   }
 }
