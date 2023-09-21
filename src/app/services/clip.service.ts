b6179afd9abee6d5d459030643b52974c1206ca1
Cristian Tudor <cristi_tcs@live.com>
2023-09-21 23:52:35 +0300

    implemented sorting clips and delete clip functionalities for the manage page

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -6,7 +6,8 @@ import {
   QuerySnapshot,
 } from '@angular/fire/compat/firestore';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { switchMap, of, map } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { switchMap, of, map, combineLatest, BehaviorSubject } from 'rxjs';
 
 import IClip from '../models/clip.model';
 
@@ -16,7 +17,11 @@ import IClip from '../models/clip.model';
 export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>;
 
-  constructor(private db: AngularFirestore, private auth: AngularFireAuth) {
+  constructor(
+    private db: AngularFirestore,
+    private auth: AngularFireAuth,
+    private storage: AngularFireStorage
+  ) {
     this.clipsCollection = db.collection('clips');
   }
 
@@ -24,14 +29,18 @@ export class ClipService {
     return this.clipsCollection.add(data);
   }
 
-  getUserClips() {
-    return this.auth.user.pipe(
-      switchMap((user) => {
+  getUserClips(sort$: BehaviorSubject<string>) {
+    return combineLatest([this.auth.user, sort$]).pipe(
+      switchMap((values) => {
+        const [user, sort] = values;
+
         if (!user) {
           return of([]);
         }
 
-        const query = this.clipsCollection.ref.where('uid', '==', user.uid);
+        const query = this.clipsCollection.ref
+          .where('uid', '==', user.uid)
+          .orderBy('timestamp', sort === '1' ? 'desc' : 'asc');
 
         return query.get();
       }),
@@ -43,5 +52,13 @@ export class ClipService {
     return this.clipsCollection.doc(id).update({
       title,
     });
+  }
+
+  async deleteClip(clip: IClip) {
+    const clipRef = this.storage.ref(`clips/${clip.fileName}`);
+
+    clipRef.delete();
+
+    await this.clipsCollection.doc(clip.docID).delete();
   }
 }
